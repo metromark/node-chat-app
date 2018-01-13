0588e21e98b2fc7a0c5f4f9c1b5151659ba1e564
metromark <markptoledo@gmail.com>
2018-01-14 07:12:49 +0800

    Chat app live

--- public/js/index.js
+++ public/js/index.js
@@ -31,11 +31,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
   
+  var messageTextbox = jQuery('[name=message]')
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function() {
-    
+    messageTextbox.val('');
   })
 })
 
@@ -45,14 +47,18 @@ locationButton.on('click', function() {
   if(!navigator.geolocation){
     return alert('Geolocation not support by your browser.')
   }
+
+  locationButton.attr('disabled', 'disabled').text('Sending location...')
   
   navigator.geolocation.getCurrentPosition(function(position) {
-    console.log(position)
+    // console.log(position)
+    locationButton.removeAttr('disabled').text('Send Location')
     socket.emit('createLocationMessage',{
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     })
   }, function() {
+    locationButton.removeAttr('disabled').text('Send Location')
     alert('Unable to fetch location')
   })
 })
