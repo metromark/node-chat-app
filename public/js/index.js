814f13a261140b264b6625fad5e6d369881991e2
metromark <markptoledo@gmail.com>
2018-01-14 05:57:44 +0800

    Add utilities: generateMessage and test case

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,8 @@ var socket = io();
 
 socket.on('connect', () => {
   console.log('Connected to server');
-
-
+  
+  
 })
 
 socket.on('disconnect', () => {
@@ -17,12 +17,6 @@ socket.on('newMessage', function(message){
   jQuery('#messages').append(li);
 })
 
-// socket.emit('createMessage', {
-//   from: 'Frank',
-//   text: 'Hi'
-// }, function(data) {
-//   console.log('Got it', data)
-// })
 
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
@@ -31,6 +25,24 @@ jQuery('#message-form').on('submit', function(e) {
     from: 'User',
     text: jQuery('[name=message]').val()
   }, function() {
+    
+  })
+})
+
+
+var locationButton= jQuery('#send-location');
+locationButton.on('click', function() {
+  if(!navigator.geolocation){
+    return alert('Geolocation not support by your browser.')
+  }
   
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position)
+    socket.emit('createLocationMessage',{
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  }, function() {
+    alert('Unable to fetch location')
   })
 })
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
+const {generateMessage} = require('./utils/message')
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000
 var app = express();
@@ -14,26 +15,21 @@ app.use(express.static(publicPath));
 io.on('connection', function (socket) {
   console.log('New user connected');
 
-    socket.emit('newMessage', {
-      from: 'Admin',
-      text: 'welcome to the chat app',
-      createdAt: new Date().getTime()
-    })
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to ChatApp'))
     
-    socket.broadcast.emit('newMessage', {
-      from: 'Admin',
-      text: 'New user joined',
-      createdAt: new Date().getTime()
-    })
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'))
 
     socket.on('createMessage', (message, callback) => {
       console.log('createMessage', message);
-      io.emit('newMessage', {
-        from: message.from,
-        text: message.text,
-        createdAt: new Date().getTime()
-      });
+      io.emit('newMessage', generateMessage(message.from, message.text));
       callback('This is from the server');
+    })
+
+    socket.on('createLocationMessage', (coords, err) => {
+      if(err){
+        return err;
+      }
+      io.emit('newMessage', generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`))
     })
 
     
